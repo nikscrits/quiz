4af3fba129165ce226af03b7600fa481f14bd0a5
Nicola Critten <ucesncr@ucl.ac.uk>
2018-04-30 07:11:49 -0400

    Downloading the questions from the server and plotting them on the map.

--- ucesncr/www/js/appActivity.js
+++ ucesncr/www/js/appActivity.js
@@ -135,3 +135,80 @@ function showPosition(position) {
 	}	
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions can use the same variable 
+	
+	var client2;
+	
+	// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
+	
+	var questionsLayer;
+
+	// create the code to get the Earthquakes data using an XMLHttpRequest
+	function getQuestions() {
+	client2 = new XMLHttpRequest();
+	client2.open('GET','http://developer.cege.ucl.ac.uk:30288/getquestions');
+	client2.onreadystatechange = questionResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
+	client2.send();
+}
+
+	// create custom red marker
+	var testMarkerRed = L.AwesomeMarkers.icon({
+	icon: 'play',
+	markerColor: 'red'
+});
+
+	// create the code to wait for the response from the data server, and process the response once it is received
+	
+	function questionResponse() {
+	
+	// this function listens out for the server to say that the data is ready - i.e. has state 4
+	
+	if (client2.readyState == 4) {
+	// once the data is ready, process the data
+	
+	var questionData = client2.responseText;
+	loadQuestionLayer(questionData);
+}
+}
+
+	// convert the received data - which is text - to JSON format and add it to the map
+	function loadQuestionLayer(questionData) {
+	
+	// convert the text to JSON
+	var questionJSON = JSON.parse(questionData);
+	
+	// load the geoJSON layer
+	var questionsLayer = L.geoJson(questionJSON,
+{
+	// use point to layer to create the points
+	pointToLayer: function (feature, latlng)
+{
+	// look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
+	// also include a pop-up that shows the place value of the earthquakes
+
+	return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.point_name +"</b>");
+
+},
+}).addTo(mymap);
+	
+	// change the map zoom so that all the data is shown
+	mymap.fitBounds(questionsLayer.getBounds());
+}
+
+
+
