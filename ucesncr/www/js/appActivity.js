cbbdf90828bac95dbd9e57452404590870bbcc55
Nicola Critten <ucesncr@ucl.ac.uk>
2018-05-02 11:40:43 -0400

    Editing on click.

--- ucesncr/www/js/appActivity.js
+++ ucesncr/www/js/appActivity.js
@@ -200,15 +200,38 @@ function deg2rad(deg) {
 
 function onClick(e) {
 
-	if (this.icon == "testMarkerGreen"){
-		alert("is green");
-	}
+	alert(this.feature.properties.question);
 
-	//alert(this.feature.properties.question);
+	callDivChange();
 }
 
 
+var  xhr;  // define the global variable to process the AJAX request 
 
+function callDivChange() {   
+	xhr = new XMLHttpRequest();
+	var filename = document.getElementById("questionpage.html").value;
+	xhr.open("GET", filename, true);   
+	xhr.onreadystatechange = processDivChange;   
+	try {      
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");   
+	}   
+	catch (e) {   
+		// this only works in internet explorer   
+	}   
+	xhr.send(); 
+	}   
+	
+function processDivChange() { 
+if (xhr.readyState < 4)       // while waiting response from server         
+	document.getElementById('mapid').innerHTML = "Loading..."; 
+	 
+	    else if (xhr.readyState === 4) {       // 4 = Response from server has been completely loaded.      
+		if (xhr.status == 200 && xhr.status < 300)     
+			// http status between 200 to 299 are all successful             
+		document.getElementById('mapid').innerHTML = xhr.responseText;     
+		} 
+} 
 
 
 
