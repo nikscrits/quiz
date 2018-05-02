a1fb57123272ddef98d1ee310a2424a99f1731cf
Nicola Critten <ucesncr@ucl.ac.uk>
2018-05-02 05:14:01 -0400

    Removing earthquake activity code.

--- ucesncr/platforms/browser/www/js/appActivity.js
+++ ucesncr/platforms/browser/www/js/appActivity.js
@@ -17,75 +17,6 @@
 	 
 	 mymap.locate({setView: true, maxZoom: 18});
 
-	// create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions can use the same variable 
-	
-	var client;
-	
-	// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
-	
-	var earthquakelayer;
-
-	// create the code to get the Earthquakes data using an XMLHttpRequest
-	function getEarthquakes() {
-	client = new XMLHttpRequest();
-	client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
-	client.onreadystatechange = earthquakeResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
-	client.send();
-}
-
-	// create custom red marker
-	var testMarkerRed = L.AwesomeMarkers.icon({
-	icon: 'play',
-	markerColor: 'red'
-});
-
-	// create custom pink marker 
-	var testMarkerPink = L.AwesomeMarkers.icon({
-	icon: 'play',
-	markerColor: 'pink'
-});
-	// create the code to wait for the response from the data server, and process the response once it is received
-	
-	function earthquakeResponse() {
-	
-	// this function listens out for the server to say that the data is ready - i.e. has state 4
-	
-	if (client.readyState == 4) {
-	// once the data is ready, process the data
-	
-	var earthquakedata = client.responseText;
-	loadEarthquakelayer(earthquakedata);
-}
-}
-
-	// convert the received data - which is text - to JSON format and add it to the map
-	function loadEarthquakelayer(earthquakedata) {
-	
-	// convert the text to JSON
-	var earthquakejson = JSON.parse(earthquakedata);
-	
-	// load the geoJSON layer
-	var earthquakelayer = L.geoJson(earthquakejson,
-{
-	// use point to layer to create the points
-	pointToLayer: function (feature, latlng)
-{
-	// look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
-	// also include a pop-up that shows the place value of the earthquakes
-	if (feature.properties.mag > 1.75) {
-	return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.place +"</b>");
-}
-	else {
-	// magnitude is 1.75 or less
-	return L.marker(latlng, {icon:testMarkerPink}).bindPopup("<b>"+feature.properties.place +"</b>");;
-}
-},
-}).addTo(mymap);
-	
-	// change the map zoom so that all the data is shown
-	mymap.fitBounds(earthquakelayer.getBounds());
-}
-
 
 //adapted from: https://www.w3schools.com/html/html5_geolocation.asp
 //adapted from: https://gis.stackexchange.com/questions/182068/getting-current-user-location-automatically-every-x-seconds-to-put-on-leaflet
@@ -134,4 +65,68 @@ function showPosition(position) {
 		
 	}	
 }
+
+
+
+	// create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions can use the same variable 
+	
+	var client2;
+	
+	// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
+	
+	var questionsLayer;
+
+	// create the code to get the Earthquakes data using an XMLHttpRequest
+	function getQuestions() {
+	client2 = new XMLHttpRequest();
+	client2.open('GET','http://developer.cege.ucl.ac.uk:30288/getquestions');
+	client2.onreadystatechange = questionResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
+	client2.send();
+}
+
+	// create custom red marker
+	var testMarkerRed = L.AwesomeMarkers.icon({
+	icon: 'play',
+	markerColor: 'red'
+});
+
+	// create the code to wait for the response from the data server, and process the response once it is received
+	
+	function questionResponse() {
+	
+	// this function listens out for the server to say that the data is ready - i.e. has state 4
+	
+	if (client2.readyState == 4) {
+	// once the data is ready, process the data
+	
+	var questionData = client2.responseText;
+	loadQuestionLayer(questionData);
+}
+}
+
+	// convert the received data - which is text - to JSON format and add it to the map
+	function loadQuestionLayer(questionData) {
+	
+	// convert the text to JSON
+	var questionJSON = JSON.parse(questionData);
+	
+	// load the geoJSON layer
+	var questionsLayer = L.geoJson(questionJSON,
+{
+	// use point to layer to create the points
+	pointToLayer: function (feature, latlng)
+{
+	// look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
+	// also include a pop-up that shows the place value of the earthquakes
+
+	return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.point_name +"</b>");
+
+},
+}).addTo(mymap);
+	
+	// change the map zoom so that all the data is shown
+	mymap.fitBounds(questionsLayer.getBounds());
+}
+
+
 
